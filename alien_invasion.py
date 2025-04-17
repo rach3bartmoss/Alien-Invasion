2ae42bb37b8c890b6488bddd9c8a022a65a3b121
Douglas Alves <rachebartm00s@proton.me>
2025-04-17 15:37:49 +0100

    add fullscreen mode and left and right movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,7 @@
-from ast import Tuple
 import sys
 import pygame
 from settings import Settings
 from ship import Ship
-from multiprocessing.dummy import Value
 
 class AlienInvasion:
 	"""Overall class to manage game assets and controls"""
@@ -13,9 +11,11 @@ class AlienInvasion:
 		self.clock = pygame.time.Clock()
 		self.settings = Settings()
 
-		self.screen = pygame.display.set_mode(
-			(self.settings.screen_width, self.settings.screen_height))
+		self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+		self.settings.screen_width = self.screen.get_rect().width
+		self.settings.screen_height = self.screen.get_rect().height
 		pygame.display.set_caption("Alien Invasion")
+
 		self.ship = Ship(self)
 
 	def	update_bg_color(self, new_color: tuple):
@@ -27,8 +27,8 @@ class AlienInvasion:
 	def	run_game(self):
 		while True:
 			self._check_events()
+			self.ship.update()
 			self._update_screen()
-
 			self.clock.tick(60)
 
 	def	_check_events(self):
@@ -36,8 +36,23 @@ class AlienInvasion:
 			if event.type == pygame.QUIT:
 				sys.exit()
 			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_ESCAPE:
-					sys.exit()
+				self._check_keydown_events(event)
+			elif event.type == pygame.KEYUP:
+				self._check_keyup_events(event)
+
+	def	_check_keydown_events(self, event):
+		if event.key == pygame.K_ESCAPE:
+			sys.exit()
+		elif event.key == pygame.K_RIGHT:
+			self.ship.moving_right = True
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = True
+
+	def	_check_keyup_events(self, event):
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = False
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = False
 
 	def	_update_screen(self):
 		self.screen.fill(self.settings.bg_color)
--- ship.py
+++ ship.py
@@ -4,6 +4,7 @@ class Ship:
 	def	__init__(self, ai_game):
 		"""The rect like methods is to convert the image to rectangle"""
 		self.screen = ai_game.screen
+		self.settings = ai_game.settings
 		self.screen_rect = ai_game.screen.get_rect()
 
 		self.image = pygame.image.load('images/ship.bmp')
@@ -11,5 +12,16 @@ class Ship:
 
 		self.rect.midbottom = self.screen_rect.midbottom
 
+		self.moving_right = False
+		self.moving_left = False
+		self.x = float(self.rect.x)
+
 	def	blitme(self):
 		self.screen.blit(self.image, self.rect)
+
+	def	update(self):
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			self.x += self.settings.ship_speed
+		if self.moving_left and self.rect.left > 0:
+			self.x -= self.settings.ship_speed
+		self.rect.x = int(self.x)
