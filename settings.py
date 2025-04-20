90c5fe47a75fa1c604035c4c7dcb69603c3ff240
Douglas Alves <rachebartm00s@proton.me>
2025-04-20 01:10:35 +0100

    new settings

--- settings.py
+++ settings.py
@@ -4,3 +4,10 @@ class Settings:
 		self.screen_height = 800
 		self.bg_color = (4, 12, 36)
 		self.ship_speed = 3.0
+
+		#Bullet Settings
+		self.bullet_speed = 5.0
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (255, 0, 0)
+		self.bullets_allowed = 10
