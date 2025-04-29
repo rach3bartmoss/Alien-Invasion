b81e29bcc01dd0638cdc880313189f40055205c8
Douglas Alves <rachebartm00s@proton.me>
2025-04-29 10:34:42 +0100

    updates

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from settings import Settings
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
+from background import Background
 
 class AlienInvasion:
 	"""Overall class to manage game assets and controls"""
@@ -19,38 +20,38 @@ class AlienInvasion:
 		self.settings.screen_height = self.screen.get_rect().height
 		pygame.display.set_caption("Alien Invasion")
 
+		self.background = Background(self)
 		self.ship = Ship(self)
 		self.bullets = pygame.sprite.Group()
 		self.aliens = pygame.sprite.Group()
 
 		self._create_fleet()
 
-	def	_create_alien(self, x_position):
+	def	_create_alien(self, x_position, y_position):
 		new_alien = Alien(self)
 		new_alien.x = x_position
 		new_alien.rect.x = x_position
+		new_alien.rect.y = y_position
 		self.aliens.add(new_alien)
 
 	def	_create_fleet(self):
 		alien = Alien(self)
-		alien_width = alien.rect.width
-		current_x = alien_width
+		alien_width, alien_height = alien.rect.size
+		current_x, current_y = alien_width, alien_height
 
-		while current_x < (self.settings.screen_width - 2 * alien_width):
-			self._create_alien(current_x)
-			current_x += 2 * alien_width
-
-	def	update_bg_color(self, new_color: tuple):
-		if not isinstance(new_color, tuple):
-			raise ValueError("error: update_bg_color() -> expects a tuple")
-		else:
-			self.settings.bg_color = new_color
+		while current_y < (self.settings.screen_height - 3 * alien_height):
+			while current_x < (self.settings.screen_width - 2 * alien_width):
+				self._create_alien(current_x, current_y)
+				current_x += 2 * alien_width
+			current_x = alien_width
+			current_y += 2 * alien_height
 
 	def	run_game(self):
 		while True:
 			self._check_events()
 			self.ship.update()
 			self._update_bullets()
+			self._update_aliens()
 			self._update_screen()
 			self.clock.tick(60)
 
@@ -81,6 +82,7 @@ class AlienInvasion:
 
 	def	_update_screen(self):
 		self.screen.fill(self.settings.bg_color)
+		self.background.blitme()
 		for bullet in self.bullets.sprites():
 			bullet.draw_bullet()
 		self.ship.blitme()
@@ -97,6 +99,21 @@ class AlienInvasion:
 		for bullet in self.bullets.copy():
 			if bullet.rect.bottom <= 0:
 				self.bullets.remove(bullet)
+
+	def	_update_aliens(self):
+		self._check_fleet_edges()
+		self.aliens.update()
+
+	def	_check_fleet_edges(self):
+		for alien in self.aliens.sprites():
+			if alien.check_edges():
+				self._change_fleet_direction()
+				break
+
+	def	_change_fleet_direction(self):
+		for alien in self.aliens.sprites():
+			alien.rect.y += self.settings.fleet_drop_speed
+		self.settings.fleet_direction *= -1
 
 if __name__ == '__main__':
 	"""Run the code above only if the file was run as main script, not imported"""
