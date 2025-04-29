09cabb0557ded4ec212fcc6481b989eba9fe7178
Douglas Alves <rachebartm00s@proton.me>
2025-04-29 10:35:02 +0100

    settings class

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings:
 		self.screen_width = 1200
 		self.screen_height = 800
 		self.bg_color = (4, 12, 36)
-		self.ship_speed = 3.0
+		self.ship_speed = 5.0
 
 		#Bullet Settings
 		self.bullet_speed = 5.0
@@ -11,3 +11,7 @@ class Settings:
 		self.bullet_height = 15
 		self.bullet_color = (255, 0, 0)
 		self.bullets_allowed = 10
+
+		self.alien_speed = 2.0
+		self.fleet_drop_speed = 10
+		self.fleet_direction = 1
